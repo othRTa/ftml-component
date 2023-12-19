d24fe60fd8a7abcdcf306eac8404acc6ed7ee4a9
RTa-technology <scpfoundationidcardmaker@gmail.com>
2023-12-20 03:43:14 +0900

    add test

---
+++ test/ftml-worker.js
@@ -0,0 +1,10 @@
+import { init, renderHTML, ready, loading } from '../lib/ftml-wasm/esm/wj-ftml-wasm.esm.js';
+
+init();
+onmessage = async (e) => {
+    if (!ready) await loading;
+    const ftmlSource = e.data.value;
+
+    const { html, styles } = renderHTML(ftmlSource);
+    postMessage({ html, styles });
+};
